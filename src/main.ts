2104c98d893aa6940453e446231c145e890edede
Oscar <trabajos1379@gmail.com>
2024-04-25 22:07:11 +0200

    funcion game over implementada, en curso funcion mePlanto

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import './style.css';
 
-let puntuacionInicial = 0;
+let puntuacion = 0;
 /* const AS = 1;
 const DOS = 2;
 const TRES = 3;
@@ -19,7 +19,7 @@ const muestraPuntuacion = () => {
     mostrarPuntuacion !== undefined &&
     mostrarPuntuacion instanceof HTMLHeadingElement
   ) {
-    mostrarPuntuacion.textContent = puntuacionInicial.toString();
+    mostrarPuntuacion.textContent = puntuacion.toString();
   } else {
     console.error('No se puede mostrar la puntuación');
   }
@@ -47,13 +47,14 @@ const muestraCarta = (valorCartaAleatoria: number): void => {
   const CABALLO = document.querySelector('.carta_copas_caballo');
   const REY = document.querySelector('.carta_copas_rey');
 
+  // Mapea el valor de la carta y la muestra
   switch (valorCartaAleatoria) {
     case 1:
       if (AS !== null && AS !== undefined && AS instanceof HTMLElement) {
         console.log('AS');
         AS.classList.add('mostrar_carta');
-        puntuacionInicial += valorCartaAleatoria;
-        muestraPuntuacion();
+        puntuacion += valorCartaAleatoria;
+        return muestraPuntuacion();
       } else {
         console.error('No se puede mostrar el AS');
       }
@@ -62,8 +63,8 @@ const muestraCarta = (valorCartaAleatoria: number): void => {
       if (DOS !== null && DOS !== undefined && DOS instanceof HTMLElement) {
         console.log('DOS');
         DOS.classList.add('mostrar_carta');
-        puntuacionInicial += valorCartaAleatoria;
-        muestraPuntuacion();
+        puntuacion += valorCartaAleatoria;
+        return muestraPuntuacion();
       } else {
         console.error('No se puede mostrar el DOS');
       }
@@ -72,8 +73,8 @@ const muestraCarta = (valorCartaAleatoria: number): void => {
       if (TRES !== null && TRES !== undefined && TRES instanceof HTMLElement) {
         console.log('TRES');
         TRES.classList.add('mostrar_carta');
-        puntuacionInicial += valorCartaAleatoria;
-        muestraPuntuacion();
+        puntuacion += valorCartaAleatoria;
+        return muestraPuntuacion();
       } else {
         console.error('No se puede mostrar el TRES');
       }
@@ -86,8 +87,8 @@ const muestraCarta = (valorCartaAleatoria: number): void => {
       ) {
         console.log('CUATRO');
         CUATRO.classList.add('mostrar_carta');
-        puntuacionInicial += valorCartaAleatoria;
-        muestraPuntuacion();
+        puntuacion += valorCartaAleatoria;
+        return muestraPuntuacion();
       } else {
         console.error('No se puede mostrar el CUATRO');
       }
@@ -100,8 +101,8 @@ const muestraCarta = (valorCartaAleatoria: number): void => {
       ) {
         console.log('CINCO');
         CINCO.classList.add('mostrar_carta');
-        puntuacionInicial += valorCartaAleatoria;
-        muestraPuntuacion();
+        puntuacion += valorCartaAleatoria;
+        return muestraPuntuacion();
       } else {
         console.error('No se puede mostrar el CINCO');
       }
@@ -110,8 +111,8 @@ const muestraCarta = (valorCartaAleatoria: number): void => {
       if (SEIS !== null && SEIS !== undefined && SEIS instanceof HTMLElement) {
         console.log('SEIS');
         SEIS.classList.add('mostrar_carta');
-        puntuacionInicial += valorCartaAleatoria;
-        muestraPuntuacion();
+        puntuacion += valorCartaAleatoria;
+        return muestraPuntuacion();
       } else {
         console.error('No se puede mostrar el SEIS');
       }
@@ -124,8 +125,8 @@ const muestraCarta = (valorCartaAleatoria: number): void => {
       ) {
         console.log('SIETE');
         SIETE.classList.add('mostrar_carta');
-        puntuacionInicial += valorCartaAleatoria;
-        muestraPuntuacion();
+        puntuacion += valorCartaAleatoria;
+        return muestraPuntuacion();
       } else {
         console.error('No se puede mostrar el SIETE');
       }
@@ -134,8 +135,9 @@ const muestraCarta = (valorCartaAleatoria: number): void => {
       if (SOTA !== null && SOTA !== undefined && SOTA instanceof HTMLElement) {
         console.log('SOTA');
         SOTA.classList.add('mostrar_carta');
-        puntuacionInicial += valorCartaAleatoria;
-        muestraPuntuacion();
+        valorCartaAleatoria = 0.5;
+        puntuacion += valorCartaAleatoria;
+        return muestraPuntuacion();
       } else {
         console.error('No se puede mostrar el SOTA');
       }
@@ -148,8 +150,9 @@ const muestraCarta = (valorCartaAleatoria: number): void => {
       ) {
         console.log('CABALLO');
         CABALLO.classList.add('mostrar_carta');
-        puntuacionInicial += valorCartaAleatoria;
-        muestraPuntuacion();
+        valorCartaAleatoria = 0.5;
+        puntuacion += valorCartaAleatoria;
+        return muestraPuntuacion();
       } else {
         console.error('No se puede mostrar el CABALLO');
       }
@@ -158,8 +161,9 @@ const muestraCarta = (valorCartaAleatoria: number): void => {
       if (REY !== null && REY !== undefined && REY instanceof HTMLElement) {
         console.log('REY');
         REY.classList.add('mostrar_carta');
-        puntuacionInicial += valorCartaAleatoria;
-        muestraPuntuacion();
+        valorCartaAleatoria = 0.5;
+        puntuacion += valorCartaAleatoria;
+        return muestraPuntuacion();
       } else {
         console.error('No se puede mostrar el REY');
       }
@@ -171,7 +175,46 @@ const muestraCarta = (valorCartaAleatoria: number): void => {
   }
 };
 
+// Mostrar mensajes en función de la puntuación
+const texto = () => {
+  let texto = document.querySelector('.texto_avisos');
+  return texto;
+};
+// Gestionar Game Over
+const gameOver = () => {
+  let mensaje = texto();
+  if (
+    mensaje !== null &&
+    mensaje !== undefined &&
+    mensaje instanceof HTMLHeadingElement &&
+    puntuacion > 7.5
+  ) {
+    mensaje.textContent = 'GAME OVER';
+    console.error('GAME OVER');
+  } else {
+    console.log('No es game over');
+  }
+};
+
+// Gestionar me planto
+const mePlanto = () => {
+  let mensaje = texto();
+  if (
+    mensaje !== null &&
+    mensaje !== undefined &&
+    mensaje instanceof HTMLHeadingElement &&
+    puntuacion < 4
+  ) {
+    mensaje.textContent = 'Has sido muy conservador';
+  }
+};
+
 const handleClick = () => {
+  eventos();
+};
+
+const eventos = () => {
+  // Boton pedir carta
   const pedirCarta = document.querySelector('.pedir_carta');
   if (
     pedirCarta !== null &&
@@ -180,10 +223,13 @@ const handleClick = () => {
   )
     pedirCarta.addEventListener('click', () => {
       dameCarta();
-      let valorCartaAleatoria = dameCarta();
-      muestraCarta(valorCartaAleatoria);
-      muestraPuntuacion();
-      console.log(valorCartaAleatoria);
+      let valorCarta = dameCarta();
+      muestraCarta(valorCarta);
+      gameOver();
+      console.log(valorCarta);
+      // Btn plantarse
+      const btnPlantarse = document.querySelector('.btn_plantarse');
+      btnPlantarse?.addEventListener('click', mePlanto);
     });
 };
 
